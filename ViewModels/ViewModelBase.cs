9b53f0947ed727e0308ba3af318fd2261639af19
sbiglia <sebastian.biglia@gmail.com>
2020-11-12 22:49:53 -0300

    Initial Commit - hello world avalonia template

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace gitbang.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}

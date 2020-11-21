1e522475b7553ca376b821026a3ad6c1c0df71f5
angryb <angryb@angryb.com>
2020-11-21 12:34:00 -0300

    WIP

--- Gitbang/App.xaml.cs
+++ Gitbang/App.xaml.cs
@@ -2,13 +2,23 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Gitbang.Services;
+using Gitbang.Services.ExecuteCommands;
+using Gitbang.Services.Queries;
 using Gitbang.ViewModels;
 using Gitbang.Views;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace Gitbang
 {
     public class App : Application
     {
+
+        private IConfiguration _configuration;
+        private IExecuter _executer;
+        private ICommandText _commandText;
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
@@ -18,11 +28,20 @@ namespace Gitbang
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                var config = new 
-                var repo = new DataRepository()
+                var builder = new ConfigurationBuilder()
+                                 .SetBasePath(Path.Combine(AppContext.BaseDirectory))
+                                 .AddJsonFile("appsettings.json", optional: true);
+
+                _configuration = builder.Build();
+
+                _commandText = new CommandText();
+                _executer = new Executer();
+
+                var repo = new DataRepository(_configuration, _commandText, _executer);
+                
                 desktop.MainWindow = new MainWindow
                 {
-                    DataContext = new MainWindowViewModel(),
+                    DataContext = new MainWindowViewModel(repo),
                 };
             }
 

52db47bf00231f29370487392a2271398d7891ab
sbiglia <sebastian.biglia@gmail.com>
2022-07-30 22:25:33 -0300

    wip
    wip

--- Gitbang.Core/Git/GitUrl.cs
+++ Gitbang.Core/Git/GitUrl.cs
@@ -17,7 +17,7 @@ namespace Gitbang.Core.Git
 
         public string Host { get; }
 
-        public string UserName { get; }
+        public string Username { get; }
 
         public string Slug { get; }
 
@@ -46,8 +46,83 @@ namespace Gitbang.Core.Git
             if (!Uri.TryCreate(uriString, UriKind.Absolute, out result))
             {
                 Host = null;
-                Protocol = GitUrl.NetworkProtocol.Other
+                Protocol = GitNetworkProtocol.Other;
+                Uri = null;
+                Username = null;
+                return;
+            }
+
+            Host = result.Host.ToLower();
+            Protocol = GetProtocol(result.Scheme);
+            Uri = result;
+            Username = ParseUsername(result);
+            var text = Uri.LocalPath.TrimStart('/');
+
+            if(text.EndsWith(".git"))
+            {
+                text = text.Substring(0, text.Length - ".git".Length);
+            }
+            Slug = text;
+
+            var array = text.Split('/');
+            if(array.Length >= 2)
+            {
+                RepositoryName = array[array.Length - 1];
+                return;
+            }
+            
+            if(array.Length == 1)
+            {
+                RepositoryName = array[0];
             }
         }
+
+        private static string ParseUsername(Uri uri)
+        {
+            if(uri.Scheme == "ssh")
+            {
+                return null;
+            }
+            
+            var userInfo = uri.UserInfo;
+            if(string.IsNullOrEmpty(userInfo))
+            {
+                return null;
+            }
+            
+            var num = userInfo.IndexOf(":");
+
+            if(num==-1)
+            {
+                return userInfo;
+            }
+
+            if(num == 0)
+            {
+                return null;
+            }
+            
+            return userInfo.Substring(0, num);
+        }
+
+        private static GitNetworkProtocol GetProtocol(string scheme)
+        {
+            switch(scheme)
+            {
+                case "https":
+                    {
+                        return GitNetworkProtocol.Https;
+                    }
+                case "ssh":
+                    {
+                        return GitNetworkProtocol.Ssh;
+                    }
+                default:
+                    return GitNetworkProtocol.Other;
+            }
+        }
+        
     }
+
+   
 }
